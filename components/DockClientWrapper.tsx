f179190337450947d5b9414a6f8f075764e22a20
zidniruf <mainbuat062@gmail.com>
2025-11-23 18:06:22 +1030

    case-sensitive rename final fix

--- components/DockClientWrapper.tsx
+++ components/DockClientWrapper.tsx
@@ -1,7 +1,7 @@
 'use client'; 
 
 import React from 'react';
-import Dock, { DockItemData } from './dock_temp'; 
+import Dock, { DockItemData } from './Dock'; 
 // Saya sesuaikan ikon agar cocok dengan menu portofolio Anda (Home, About, Skills, Portfolio, Contact)
 import { Home as HomeIcon, User, Code, Image as ImageIcon, Mail } from 'lucide-react';
 
